f88129941ba2ec9da7fac689b2c74c5cf6681512
enzo <enzo@Notenzo>
2019-06-25 22:16:46 -0300

    Commit com as pastas com o javafx

--- ProjetoPetsPOO/src/controllers/ListarRelatorioController.java
+++ ProjetoPetsPOO/src/controllers/ListarRelatorioController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -16,9 +17,21 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ListarRelatorioController implements Initializable {
+	@FXML
+    public Button buttonRelAnimaisAdotados;
+	@FXML
+    public Button buttonItensDoados;
+	@FXML
+    public Button buttonRelFluxoCaixa;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
+		Context c = Context.getInstance();
+    	Pessoa usuario = c.getUsuarioAtual();
+    	if(usuario.getPesTipo() != 1) {
+    		buttonRelAnimaisAdotados.setDisable(true);
+    		buttonItensDoados.setDisable(true);
+    		buttonRelFluxoCaixa.setDisable(true);
+    	}
 	}
 }
